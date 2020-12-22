16e3e1bda2fec66a2201ff112fdd7d8477285721
MedaniGunathilaka <hansikamedani@gmail.com>
2020-12-22 18:32:34 +0530

    Added a code on fibonacci series

---
+++ fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int Fibonacci( int ) ;
+
+void main(void)
+  {
+    int n ;
+    printf("Input a number \n");
+    scanf("%d", &n);
+    printf("%d \n", Fibonacci(n) ) ;
+}
+
+int Fibonacci( int n )
+{
+  int Result ;
+  if ( n == 0 )
+    Result = 0 ;
+  else
+    if ( n == 1)
+      Result = 1;
+    else 
+      Result = Fibonacci( n-1 ) + Fibonacci(n-2);
+  return( Result ) ;
+}
+
+
